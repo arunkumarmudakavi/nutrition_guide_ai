63b68a66d2df7e372cbf167ea001476f116bc246
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2025-07-23 23:52:25 +0530

    url changed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -58,7 +58,7 @@ export const authOptions: NextAuthOptions = {
         }
     },
     pages: {
-        signIn: "/login",
+        signIn: "/signin",
         error: "/error"
     },
     session: {
